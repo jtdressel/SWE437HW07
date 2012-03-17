d9e1b4e0416f89b217c2848bfd91e1ebabe7dbf7
James Dressel <ubergeek@jamesdressel.com>
2012-03-17 19:55:03 -0400

    FEATURE: Add skelten test class

---
+++ src/dresselJames07.java
@@ -0,0 +1,21 @@
+//dresselJames07.java
+//A
+//b
+//c
+//d
+//e
+//f
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class dresselJames07 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
