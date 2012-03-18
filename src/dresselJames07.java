7420881fdcd339703655c99858c8d3965f798c21
James Dressel <ubergeek@jamesdressel.com>
2012-03-18 17:29:08 -0400

    DOC: Move questions for ease of use

--- src/dresselJames07.java
+++ src/dresselJames07.java
@@ -1,19 +1,4 @@
 //dresselJames07.java
-//A Identify the fault:
-//b If possible, identify a test case that does not execute the fault.
-//c If possible, identify a test case that executes the fault, but does not result in an error state
-//d if possible identify a test case that results in an error, but not a failure. Hint: Don't forget about the program counter
-//e For the given test case, identify the first error state. Be sure to describe the complete state. 
-//f Fix the fault and verify that the given test now produces the expected output. 
-
-//A
-//B
-//C
-//D
-//E
-//F
-
-
 
 //Fault: static defect
 //Error: incorrect interal state
@@ -26,16 +11,24 @@ import org.junit.Test;
 
 public class dresselJames07 {
 	
-	/*Part A
+	/*Part A Identify the fault:
 	 * The fault is that Vehicle's clone returns new Vehicle(this.x) instead of 
 	 * using super.clone. The result is that Object's clone is never called, so
 	 * the result is not of the correct class, and thus, cannot be cast into a 
 	 * Truck.   
 	 */
 
+	//Part B If possible, identify a test case that does not execute the fault.
+	
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+
+	//Part D if possible identify a test case that results in an error, but not a failure. Hint: Don't forget about the program counter
+	//Part E For the given test case, identify the first error state. Be sure to describe the complete state. 
+	//Part F Fix the fault and verify that the given test now produces the expected output. 
+	
 
 }
