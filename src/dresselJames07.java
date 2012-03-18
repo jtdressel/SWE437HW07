2da27fbe27e549cf91beef1276eda43b7f36a8aa
James Dressel <ubergeek@jamesdressel.com>
2012-03-18 17:22:27 -0400

    DOC: Added the homework guidelines to the file for ease of use

--- src/dresselJames07.java
+++ src/dresselJames07.java
@@ -1,4 +1,11 @@
 //dresselJames07.java
+//A Identify the fault:
+//b If possible, identify a test case that does not execute the fault.
+//c If possible, identify a test case that executes the fault, but does not result in an error state
+//d if possible identify a test case that results in an error, but not a failure. Hint: Don't forget about the program counter
+//e For the given test case, identify the first error state. Be sure to describe the complete state. 
+//f Fix the fault and verify that the given test now produces the expected output. 
+
 //A
 //b
 //c
