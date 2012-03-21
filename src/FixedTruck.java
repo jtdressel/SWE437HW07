adb921997d56fb4e861ed2967a723333b50316fa
James Dressel <ubergeek@jamesdressel.com>
2012-03-20 21:50:48 -0400

    FEATURE: Add equals to FixedTruck
    Not really needed, but it felt wrong leaving it without an equals.

--- src/FixedTruck.java
+++ src/FixedTruck.java
@@ -10,5 +10,28 @@ class FixedTruck extends FixedVehicle {
       ((FixedTruck) result).y = this.y;  // throws ClassCastException
       return result;             
    }
+   /* Code below this point is not strictly needed for the assignment, and thus
+    * if you want to grade quickly you can safely ignore it. I felt that my 
+    * clone tests would be inadequate if they did not check equality between 
+    * FixedTrucks. The default equals() does not work well, so I wrote my own 
+    * below. Its basically a copy of Item 8 in Bloch's Effective Java. ( I do 
+    * realize that I should also override .hashCode, but I did not want to add 
+    * a ton of extra code.)
+    */
+   @Override
+   public boolean equals(Object o){
+	   if (this==o){
+		   return true;
+	   }
+	   if (!(o instanceof FixedTruck)){
+		   return false;
+	   }
+	   FixedTruck test = (FixedTruck)o;
+	   
+	   if(this.y!=test.y){
+		   return false;
+	   }
+	   return true;
+   }
    // other methods omitted
 }
